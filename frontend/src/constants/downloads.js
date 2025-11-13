8556cc222fd34a84129b3195e9ac697563be9477
鄭宇彤 <belle86867288@gmail.com>
2025-11-14 00:47:18 +0800

    qrcode update

--- frontend/src/constants/downloads.js
+++ frontend/src/constants/downloads.js
@@ -1,2 +1,2 @@
-export const ANDROID_DOWNLOAD_URL = "https://play.google.com/store/apps/details?id=tw.gov.digitalwallet";
-export const IOS_DOWNLOAD_URL = "https://apps.apple.com/tw/app/id6446202653";
+export const ANDROID_DOWNLOAD_URL = "https://play.google.com/store/apps/details?id=tw.gov.moda.diw&hl=zh_TW";
+export const IOS_DOWNLOAD_URL = "https://testflight.apple.com/join/gxr3mQFj";
