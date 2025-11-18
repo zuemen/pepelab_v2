db3529ffd15cd266e0823b1eb4d61938ffb9b903
zuemen <147117593+zuemen@users.noreply.github.com>
2025-11-18 15:04:23 +0800

    Handle system reset preflight requests

---
+++ frontend/src/constants/downloads.js
@@ -0,0 +1,2 @@
+export const ANDROID_DOWNLOAD_URL = "https://play.google.com/store/apps/details?id=tw.gov.digitalwallet";
+export const IOS_DOWNLOAD_URL = "https://apps.apple.com/tw/app/id6446202653";
